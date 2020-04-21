39d8d9470946c675415ae6006f85e51e29225be9
zhaoguanhua <zhaogh@hdsxtech.com>
2020-04-22 00:26:56 +0800

    feat: 增加波士顿房价预测模型

--- keras/Boston_price.py
+++ keras/Boston_price.py
@@ -11,11 +11,23 @@
 from keras.models import Sequential
 from keras.layers import Dense,Activation
 from keras.datasets import boston_housing
+import matplotlib.pyplot as plt
 import numpy as np
+
+def smooth_curve(points,factor=0.9):
+    smoothed_points=[]
+    for point in points:
+        if  smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous*factor+point*(1-factor))
+        else:
+            smoothed_points.append(point)
+
+    return smoothed_points
 
 def build_model():
     model=Sequential()
-    model.add(Dense(64,activation='relu',input_dim=13))
+    model.add(Dense(64,activation='relu',input_shape=(x_train.shape[1],)))
     model.add(Dense(64,activation='relu'))
     model.add(Dense(1))
 
@@ -34,8 +46,9 @@ if __name__ == '__main__':
 
     k = 4
     num_val_samples = len(x_train) // k
-    num_epochs = 100
-    all_scores = []
+    num_epochs = 500
+    #all_scores = []
+    all_mae_histories =[]
     for i in range(k):
       print('processing fold #', i)
       # Prepare the validation data: data from partition # k
@@ -50,31 +63,30 @@ if __name__ == '__main__':
       # Build the Keras model (already compiled)
       model = build_model()
       # Train the model (in silent mode, verbose=0)
-      model.fit(partial_train_data, partial_train_targets,epochs=num_epochs, batch_size=1, verbose=0)
-      # Evaluate the model on the validation data
-      val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
-      all_scores.append(val_mae)
+      history=model.fit(partial_train_data, partial_train_targets,validation_data=(val_data,val_targets),
+                        epochs=num_epochs, batch_size=1, verbose=0)
+      print(history.history.keys())
+      mae_history = history.history['val_mean_absolute_error']
+      all_mae_histories.append(mae_history)
+      # # Evaluate the model on the validation data
+      # val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
+      # all_scores.append(val_mae)
 
-    print(all_scores)
-    print(np.mean(all_scores))
-# print(x_train[0],y_train[0])
-# print(x_test[0],y_test[0])
-#
-# model = Sequential()
-# #设置全连接层，输入为13 输入为1
-# model.add(Dense(5,input_dim=13))
-# print(model.layers[0].get_weights())
-#
-# model.add(Activation('relu'))
-# model.add(Dense(1))
-#
-# #编译模型
-# model.compile(loss='mean_squared_error',optimizer='sgd')
-# #训练模型
-# print('Training -----------')
-# for step in range(301):
-#     cost = model.train_on_batch(x_train, y_train) # Keras有很多开始训练的函数，这里用train_on_batch（）
-#     if step % 100 == 0:
-#         print('train cost: ', cost)
+    # print(all_scores)
+    # print(np.mean(all_scores))
+
+    average_mae_history=[np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+
+    plt.plot(range(1,len(average_mae_history)+1),average_mae_history)
+    plt.xlabel('Epoches')
+    plt.ylabel('Validation MAE')
+    plt.show()
+
+    smooth_mae_history = smooth_curve(average_mae_history[10:])
+
+    plt.plot(range(1,len(smooth_mae_history)+1),smooth_mae_history)
+    plt.xlabel('Epochs')
+    plt.ylabel('Validation MAE')
+    plt.show()
 
 
