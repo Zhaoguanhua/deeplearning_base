483f11a45876df72c075449b6160d687fff49c90
zhaoguanhua <zhaogh@hdsxtech.com>
2020-04-22 11:27:43 +0800

    feat: 过拟合应对策略出图对比

--- keras/IMDB_test.py
+++ keras/IMDB_test.py
@@ -30,7 +30,6 @@ def base_model():
     model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
-
 def Smaller_model():
     model = models.Sequential()
     model.add(layers.Dense(4, activation='relu', input_shape=(10000,)))
@@ -101,6 +100,8 @@ partial_x_train = x_train[10000:]
 y_val = y_train[:10000]
 partial_y_train = y_train[10000:]
 
+#***********************************************************************************************************
+#基本网络
 #定义模型1
 model = base_model()
 #编译模型
@@ -114,45 +115,45 @@ val_loss_values = history_dict['val_loss']
 #***********************************************************************************************************
 #测试减少网络规模
 #定义模型2
-# model2=Smaller_model()
-# #训练模型2
-# history2 = model2.fit(partial_x_train,partial_y_train,epochs=20,batch_size=512,validation_data=(x_val,y_val))
-# history_dict2 = history2.history
-# loss_values2 = history_dict2['loss']
-# val_loss_values2 = history_dict2['val_loss']
+model2=Smaller_model()
+#训练模型2
+history2 = model2.fit(partial_x_train,partial_y_train,epochs=20,batch_size=512,validation_data=(x_val,y_val))
+history_dict2 = history2.history
+loss_values2 = history_dict2['loss']
+val_loss_values2 = history_dict2['val_loss']
 
 #***********************************************************************************************************
 #测试L2权重正则化
 #定义模型3
-# model3 = L2_model()
-# #训练模型3
-# history3 = model3.fit(partial_x_train,partial_y_train,epochs=20,batch_size=512,validation_data=(x_val,y_val))
-# history_dict3 = history3.history
-# loss_values3 = history_dict3['loss']
-# val_loss_values3 = history_dict3['val_loss']
+model3 = L2_model()
+#训练模型3
+history3 = model3.fit(partial_x_train,partial_y_train,epochs=20,batch_size=512,validation_data=(x_val,y_val))
+history_dict3 = history3.history
+loss_values3 = history_dict3['loss']
+val_loss_values3 = history_dict3['val_loss']
 
 #***********************************************************************************************************
 #测试L1权重正则化
 #定义模型4
-# model4=L1_model()
-# #训练模型4
-# history4 = model4.fit(partial_x_train,partial_y_train,epochs=20,batch_size=512,validation_data=(x_val,y_val))
-# history_dict4 = history4.history
-# loss_values4 = history_dict4['loss']
-# val_loss_values4 = history_dict4['val_loss']
+model4=L1_model()
+#训练模型4
+history4 = model4.fit(partial_x_train,partial_y_train,epochs=20,batch_size=512,validation_data=(x_val,y_val))
+history_dict4 = history4.history
+loss_values4 = history_dict4['loss']
+val_loss_values4 = history_dict4['val_loss']
 
 #***********************************************************************************************************
 #测试L1_L2权重正则化
 #定义模型5
-# model5=L1_L2_model()
-# #训练模型4
-# history5 = model5.fit(partial_x_train,partial_y_train,epochs=20,batch_size=512,validation_data=(x_val,y_val))
-# history_dict5 = history5.history
-# loss_values5 = history_dict5['loss']
-# val_loss_values5 = history_dict5['val_loss']
+model5=L1_L2_model()
+#训练模型4
+history5 = model5.fit(partial_x_train,partial_y_train,epochs=20,batch_size=512,validation_data=(x_val,y_val))
+history_dict5 = history5.history
+loss_values5 = history_dict5['loss']
+val_loss_values5 = history_dict5['val_loss']
 
 #***********************************************************************************************************
-#测试L1_L2权重正则化
+#测试Dropout
 #定义模型6
 model6=Dropout_model()
 #训练模型4
@@ -162,9 +163,14 @@ loss_values6 = history_dict6['loss']
 val_loss_values6 = history_dict6['val_loss']
 
 epochs = range(1,len(loss_values)+1)
-#对比两种模型的验证损失
-plt.plot(epochs,val_loss_values,'b',label='Original model')
-plt.plot(epochs,val_loss_values6,'r',label='Dropout-regularized model')
+
+#对比几种模型的验证损失
+plt.plot(epochs,val_loss_values,'r',label='Original model')
+plt.plot(epochs,val_loss_values2,'g',label='Smaller model')
+plt.plot(epochs,val_loss_values3,'b',label='L2-regularized model')
+plt.plot(epochs,val_loss_values4,'r+',label='L1-regularized model')
+plt.plot(epochs,val_loss_values5,'g+',label='L1_L2-regularized model')
+plt.plot(epochs,val_loss_values6,'b+',label='Dropout-regularized model')
 plt.title('validation loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
