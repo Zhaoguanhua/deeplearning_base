1e0b2bc7a28c62e13f4a5bf2b20ef3760b84bb88
Zhaoguanhua <1594996716@qq.com>
2020-04-24 16:14:31 +0800

    feat: 下载内置的resnet预训练模型

--- keras/resnet50.py
+++ keras/resnet50.py
@@ -9,6 +9,20 @@
 """
 
 import keras
+#
+#
+# keras.utils.plot_model(keras.applications.ResNet50(include_top=True,input_shape=(224,224,3),
+#                                                    weights=None))
 
-keras.utils.plot_model(keras.applications.ResNet50(include_top=True,input_shape=(224,224,3),
-                                                   weights=None))
+from keras_applications import resnet,resnet_v2
+
+resnet_v2.ResNet152V2(include_top=True,
+                 weights='imagenet',
+                 input_tensor=None,
+                 input_shape=None,
+                 pooling=None,
+                 backend=keras.backend,
+                 layers=keras.layers,
+                 models=keras.models,
+                 utils=keras.utils
+                 )
